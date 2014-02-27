bfc9f5f5349fee0f5a161bc2d3f96dea33efd0e0
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2014-02-27 09:57:23 +0000

    Oops, fix copied test logic.

--- src/app/code/community/Meanbee/NewCategory/Test/Model/Auto/Category/New.php
+++ src/app/code/community/Meanbee/NewCategory/Test/Model/Auto/Category/New.php
@@ -11,7 +11,7 @@ class Meanbee_NewCategory_Test_Model_Auto_Category_New extends EcomDev_PHPUnit_T
         $category_id = 3;
         $date = Mage::app()->getLocale()->storeDate(Mage::app()->getStore(), "2014-02-27 12:00:00", true);
 
-        $auto_category = $this->getModelMock('meanbee_salecategory/auto_category_new', array('getCurrentDate'));
+        $auto_category = $this->getModelMock('meanbee_newcategory/auto_category_new', array('getCurrentDate'));
         $auto_category
             ->expects($this->any())
             ->method('getCurrentDate')
